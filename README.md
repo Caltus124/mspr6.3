fdbd52996db2c70e4908a31dae837ddc7c0e9be8
Caltus124 <3enzo24330@gmail.com>
2024-04-11 11:24:27 +0200

    ok

--- README.md
+++ README.md
@@ -1,6 +1,65 @@
 # mspr6.3
 
-Connexion BDD MySQL en ligne de commande
+## Installation
+
+### Docker Installation
+
+To install the necessary components using Docker, execute the following command:
+
+```bash
+sudo docker-compose up -d
 ```
+
+### Database Setup
+
+Once Docker is up and running, execute the following steps to set up the database and create an account for Grafana:
+
+1. Access the Docker container for the database:
+   ```bash
+   sudo docker exec -it db bash
+   ```
+
+2. Run the initialization script:
+   ```bash
+   ./init_db.sh
+   ```
+
+3. Log in to MySQL:
+   ```bash
+   mysql -u root -p
+   ```
+
+4. Inside the MySQL shell, execute the following commands to create a user for Grafana and grant necessary privileges:
+   ```sql
+   CREATE USER 'grafana'@'IP-DB' IDENTIFIED BY 'admin';
+   GRANT SELECT ON app_db.* TO 'grafana'@'IP-DB';
+   FLUSH PRIVILEGES;
+   ```
+   Replace `'IP-DB'` with the appropriate IP address or hostname of your database server.
+
+## Connection
+
+### Accessing PHPMyAdmin
+
+You can access PHPMyAdmin by navigating to the following URL in your web browser:
+```
+http://localhost:8081
+```
+
+### Accessing Grafana
+
+To access Grafana, use the following URL:
+```
+http://localhost:3000
+```
+
+### Connecting to MySQL Database via Command Line
+
+You can connect to the MySQL database using the following command:
+```bash
 mysql -h 127.0.0.1 -P 6033 -u admin -p
 ```
+
+Make sure to replace `'admin'` with the appropriate username if different.
+
+Please ensure that Docker is running and configured properly before attempting to access these services. For detailed instructions on setting up Docker and troubleshooting common issues, refer to the Docker documentation.
--- init.sql
+++ init.sql
@@ -1,43 +1,123 @@
 USE app_db;
 
 DROP TABLE IF EXISTS licence;
+DROP TABLE IF EXISTS type_end;
 DROP TABLE IF EXISTS prestataire;
-DROP TABLE IF EXISTS users;
+DROP TABLE IF EXISTS techniciens;
 DROP TABLE IF EXISTS password;
+DROP TABLE IF EXISTS metric;
+DROP TABLE IF EXISTS instance;
+DROP TABLE IF EXISTS redemarage;
+DROP TABLE IF EXISTS problemes;
+DROP TABLE IF EXISTS info_instance;
 
-CREATE TABLE licence (
-  id_licence int PRIMARY KEY AUTO_INCREMENT,
-  id_instance int,
-  start_date date,
-  end_date date NOT NULL
+CREATE TABLE `licence` (
+  `id_licence` int PRIMARY KEY AUTO_INCREMENT,
+  `id_instance` int,
+  `start_date` date,
+  `end_date` date NOT NULL,
+  `id_type_end` int
 );
 
-CREATE TABLE prestataire (
-  id_prestataire int PRIMARY KEY AUTO_INCREMENT,
-  nom_societe varchar(100) NOT NULL,
-  num_tel varchar(20),
-  index_tel varchar(5),
-  mail varchar(200),
-  siret varchar(50),
-  address varchar(200),
-  num_voix int(4),
-  ville varchar(200),
-  code_postal int(7)
+CREATE TABLE `type_end` (
+  `id_type_end` int PRIMARY KEY AUTO_INCREMENT,
+  `finally_type_end` enum('rupture', 'end contrat'),
+  `reel_date_end` date
 );
 
-CREATE TABLE users (
-  id_utilisateur int PRIMARY KEY AUTO_INCREMENT,
-  id_prestataire int,
-  nom varchar(50),
-  prenom varchar(50),
-  num_tel varchar(20),
-  index_tel varchar(5),
-  mail varchar(100),
-  username varchar(50) UNIQUE NOT NULL,
-  id_password int UNIQUE NOT NULL
+CREATE TABLE `prestataire` (
+  `id_prestataire` int PRIMARY KEY AUTO_INCREMENT,
+  `nom_societe` varchar(100) NOT NULL,
+  `num_tel` varchar(20),
+  `index_tel` varchar(5),
+  `mail` varchar(200),
+  `siret` varchar(50),
+  `address` varchar(200),
+  `num_voix` int(4),
+  `ville` varchar(200),
+  `code_postal` int(7)
 );
 
-CREATE TABLE password (
-  id_password int PRIMARY KEY AUTO_INCREMENT,
-  hash_pass varchar(254) NOT NULL
+CREATE TABLE `techniciens` (
+  `id_utilisateur` int PRIMARY KEY AUTO_INCREMENT,
+  `id_prestataire` int,
+  `nom` varchar(50),
+  `prenom` varchar(50),
+  `num_tel` varchar(20),
+  `index_tel` varchar(5),
+  `mail` varchar(100),
+  `username` varchar(50) UNIQUE NOT NULL,
+  `id_password` int UNIQUE NOT NULL
 );
+
+CREATE TABLE `password` (
+  `id_password` int PRIMARY KEY AUTO_INCREMENT,
+  `hash_pass` varchar(254) NOT NULL
+);
+
+CREATE TABLE `instance` (
+  `id_instance` int PRIMARY KEY AUTO_INCREMENT,
+  `nom` varchar(200) NOT NULL,
+  `num_serie` varchar(200) UNIQUE NOT NULL,
+  `storage_type` enum('SSD', 'HDD', 'SSHD', 'SATA', 'SAS') NOT NULL,
+  `version_os` varchar(20) NOT NULL,
+  `version_harvester` varchar(20) NOT NULL,
+  `IP` varchar(40) NOT NULL,
+  `IP_VPN` varchar(40),
+  `status_harvester` enum('oui', 'non'),
+  `state` enum('connecte', 'deconnecte', 'inconnu')
+);
+
+
+CREATE TABLE `metric` (
+  `id_metric` int PRIMARY KEY AUTO_INCREMENT,
+  `id_instance` int,
+  `ram_usage` float,
+  `cpu_usage` float,
+  `storage_used` float,
+  `heure` timestamp
+);
+
+CREATE TABLE `redemarage` (
+  `id_redemarage` int PRIMARY KEY AUTO_INCREMENT,
+  `id_instance` int,
+  `heure_redemarage` datetime,
+  `log_redemarage` text,
+  `heure_de_rentree` timestamp
+);
+
+CREATE TABLE `problemes` (
+  `id_problemes` int PRIMARY KEY AUTO_INCREMENT,
+  `id_instance` int,
+  `id_techniciens` int,
+  `heure_pb` datetime,
+  `log_pb` text
+);
+
+CREATE TABLE `info_instance` (
+  `id_info_instance` int PRIMARY KEY AUTO_INCREMENT,
+  `id_instance` int,
+  `location` enum('oui', 'non'),
+  `restitue` enum('oui', 'non'),
+  `id_loueur` int
+);
+
+ALTER TABLE `licence` ADD FOREIGN KEY (`id_instance`) REFERENCES `instance` (`id_instance`);
+
+ALTER TABLE `licence` ADD FOREIGN KEY (`id_type_end`) REFERENCES `type_end` (`id_type_end`);
+
+ALTER TABLE `techniciens` ADD FOREIGN KEY (`id_prestataire`) REFERENCES `prestataire` (`id_prestataire`);
+
+ALTER TABLE `techniciens` ADD FOREIGN KEY (`id_password`) REFERENCES `password` (`id_password`);
+
+ALTER TABLE `metric` ADD FOREIGN KEY (`id_instance`) REFERENCES `instance` (`id_instance`);
+
+ALTER TABLE `redemarage` ADD FOREIGN KEY (`id_instance`) REFERENCES `instance` (`id_instance`);
+
+ALTER TABLE `problemes` ADD FOREIGN KEY (`id_instance`) REFERENCES `instance` (`id_instance`);
+
+ALTER TABLE `problemes` ADD FOREIGN KEY (`id_techniciens`) REFERENCES `techniciens` (`id_utilisateur`);
+
+ALTER TABLE `info_instance` ADD FOREIGN KEY (`id_instance`) REFERENCES `instance` (`id_instance`);
+
+ALTER TABLE `info_instance` ADD FOREIGN KEY (`id_loueur`) REFERENCES `prestataire` (`id_prestataire`);
